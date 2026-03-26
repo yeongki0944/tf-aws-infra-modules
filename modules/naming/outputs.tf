a0301279e3aa4c16a2604d654f23d6aec17d98c8
younggikim <younggikim@mz.co.kr>
2026-03-26 14:57:12 +0900

    Fix: align module name with outputs reference

--- modules/naming/outputs.tf
+++ modules/naming/outputs.tf
@@ -1,21 +1,19 @@
-# tf-aws-infra-modules/modules/naming/outputs.tf
-
 output "prefix" {
   description = "조합된 표준 프리픽스 "
-  value       = module.remote_naming.prefix
+  value       = module.naming_standard.prefix
 }
 
 output "result" {
   description = "최종 리소스 이름 "
-  value       = module.remote_naming.result
+  value       = module.naming_standard.result
 }
 
 output "tags" {
   description = "MSP 표준 공통 태그 세트 "
-  value       = module.remote_naming.tags
+  value       = module.naming_standard.tags
 }
 
 output "tags_with_name" {
   description = "Name 태그가 포함된 전체 태그 세트 "
-  value       = module.remote_naming.tags_with_name
+  value       = module.naming_standard.tags_with_name
 }
